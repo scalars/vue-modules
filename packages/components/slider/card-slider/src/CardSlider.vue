58497ccd7269f8e7d30937f654071939cecbccba
Carlos Coral <coralkmar@gmail.com>
2020-04-29 09:58:37 -0500

    Created project card-slider

---
+++ packages/components/slider/card-slider/src/CardSlider.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        CardSlider
+    </div>
+</template>
+
+<script>
+export default {
+    name: "card-slider"
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ packages/storybook/stories/slider/card-slider.stories.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import CardSlider from "../../../components/slider/card-slider/src/CardSlider.vue";
+
+export default {
+    title: 'slider/CardSlider',
+    component: CardSlider
+};
+
+export const Default = () => ({
+    components: { CardSlider },
+    template: '<card-slider/>'
+});
