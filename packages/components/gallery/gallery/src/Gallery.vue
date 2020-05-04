be500b83d18ea0c405611372a178d512d71ddf14
Carlos Coral <coralkmar@gmail.com>
2020-05-04 16:23:50 -0500

    Created project gallery

---
+++ packages/components/gallery/gallery/src/Gallery.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Gallery
+    </div>
+</template>
+
+<script>
+export default {
+    name: "gallery",
+    props: {
+        images: { required: true, type: Array }
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ packages/storybook/stories/gallery/gallery.stories.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import Gallery from "../../../components/gallery/gallery/src/Gallery.vue";
+
+export default {
+    title: 'gallery/Gallery',
+    component: Gallery
+};
+
+export const Default = () => ({
+    components: { Gallery },
+    template: '<gallery/>'
+});
